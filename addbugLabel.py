009431d7b339952fbc0b0106e6e15c8497586f94
DvirHayat <dvir.chayat@gmail.com>
2025-10-05 00:33:25 +0300

    labeler on PR comment

---
+++ addbugLabel.py
@@ -0,0 +1,11 @@
+import os
+from ghapi import GhApi
+owner,repo = os.getenv("GITHUB_REPOSITORY").split("/")
+api = GhApi(owner=owner, repo=repo)
+commeted = os.getenv("INPUT_COMMAND")
+if commeted == "/bug":
+    api.issues.add_labels(issue_number=int(os.getenv("GITHUB_ISSUE_NUMBER")), labels=["bug"])
+elif commeted == "/feature":
+    api.issues.add_labels(issue_number=int(os.getenv("GITHUB_ISSUE_NUMBER")), labels=["feature"])
+elif commeted == "/resolved":
+    api.issues.remove_label(issue_number=int(os.getenv("GITHUB_ISSUE_NUMBER")), name="bug")
